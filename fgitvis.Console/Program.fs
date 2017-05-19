621dac5e0270f5857e40bd56583582812493c606
Pat Hermens <p@hermens.com.au>
2017-05-20 00:20:06 +0200

    Initial commit with all the basics

---
+++ fgitvis.Console/Program.fs
@@ -0,0 +1,65 @@
+﻿open System
+open Fake.Git
+open fgitvis
+open FSharp.Charting
+open System.Windows.Forms
+
+[<EntryPoint>]
+let main argv = 
+
+    printfn " ***************************************************************************"
+    printfn " *                                                                         *"
+    printfn " *   ########\  ######\  ######\ ########\ ##\    ##\ ######\  ######\     *"
+    printfn " *   ##  _____|##  __##\ \_##  _|\__##  __|## |   ## |\_##  _|##  __##\    *"
+    printfn " *   ## |      ## /  \__|  ## |     ## |   ## |   ## |  ## |  ## /  \__|   *"
+    printfn " *   #####\    ## |####\   ## |     ## |   \##\  ##  |  ## |  \######\     *"
+    printfn " *   ##  __|   ## |\_## |  ## |     ## |    \##\##  /   ## |   \____##\    *"
+    printfn " *   ## |      ## |  ## |  ## |     ## |     \###  /    ## |  ##\   ## |   *"
+    printfn " *   ## |      \######  |######\    ## |      \#  /   ######\ \######  |   *"
+    printfn " *   \__|       \______/ \______|   \__|       \_/    \______| \______/    *"
+    printfn " *                                                                         *"
+    printfn " ***************************************************************************"
+    printfn ""
+    let now = DateTime.UtcNow.TimeOfDay
+    printfn "Welcome to fgitvis. The time is %02d:%02d:%02d" now.Hours now.Minutes now.Seconds
+
+    // Get some input for repo and file limit
+    let repo = UserInput.requestRepositoryPath
+    let fileLimit = UserInput.requestFileLimit
+    
+    // Set up the 'git' command - thanks FAKE
+    let gitWrapper cmd = CommandHelper.getGitResult repo cmd
+    let git command = Printf.kprintf gitWrapper command
+
+    // Get the details from the local repository
+    let getCommits () = git "rev-list HEAD"
+    let getFiles commit = git "diff-tree --no-commit-id --name-only -r %s" commit
+    let limit sequence = 
+        match fileLimit with
+        | 0 -> sequence
+        | _ -> Seq.truncate fileLimit sequence
+
+    let topFiles = 
+        getCommits ()
+        |> Seq.collect getFiles
+        |> Seq.countBy id
+        |> Seq.sortByDescending snd
+        |> limit
+    
+    // Build and show the chart
+    let buildChart (data: seq<string * int>) =
+        Chart.Column(data, Name = "Commit counts")
+            .WithTitle("Files by commit count", InsideArea = false)
+            .WithXAxis(LabelStyle = ChartTypes.LabelStyle(Angle = -45, Interval = 1.0))
+            .WithYAxis(Title="# commits")
+    let showChart (chart: ChartTypes.GenericChart) =
+        Application.Run(chart.ShowChart())
+        
+    topFiles 
+    |> buildChart 
+    |> showChart
+
+    // ... and exit
+    printfn ""
+    printfn "Thanks for using fgitvis."
+    0
---
+++ fgitvis.Console/UserInput.fs
@@ -0,0 +1,19 @@
+﻿[<AutoOpen>]
+module fgitvis.UserInput
+
+open System
+open System.IO
+open Fake
+
+let requestRepositoryPath = 
+    let userInput = UserInputHelper.getUserInput "Please enter a path to a Git repo: "
+    if (String.IsNullOrWhiteSpace userInput) || not (Directory.Exists userInput) then
+        failwith "The provided input was not a valid path to a directory."
+    userInput
+    
+let requestFileLimit =
+    let userInput = UserInputHelper.getUserInput "Please enter a numeric limit to the number of files we should graph: "
+    let success, limit = Int32.TryParse(userInput)
+    if (not success) || limit < 0 then
+        failwith "Limit must be a positive integer (or 0 if you want to show all files)."
+    limit
