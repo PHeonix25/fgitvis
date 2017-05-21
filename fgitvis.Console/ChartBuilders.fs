e21452bbad28df86132e9c4905641391665fec4b
PHeonix25 <p@hermens.com.au>
2017-05-21 19:05:03 +0200

    Add another ChartBuilder

--- fgitvis.Console/ChartBuilders.fs
+++ fgitvis.Console/ChartBuilders.fs
@@ -9,7 +9,16 @@ let internal columnChartBuilder (data: seq<string * int>, name: string, title: s
         .WithXAxis(LabelStyle = ChartTypes.LabelStyle(Angle = -45, Interval = 1.0))
         .WithYAxis(Title = yAxis)
 
+let internal lineChartBuilder (data: seq<string * int>, name: string, title: string, yAxis: string) =
+    Chart.Line(data, Name = name)
+        .WithTitle(title, InsideArea = false)
+        .WithXAxis(LabelStyle = ChartTypes.LabelStyle(Angle = -45, Interval = 1.0))
+        .WithYAxis(Title = yAxis)
+
 let filesByCommitCountChart data =
     columnChartBuilder (data, "Commit counts", "Files by commit count", "# commits")
+
+let filesOverTime data =
+    lineChartBuilder (data, "Modified files", "File modification by time", "# modifications")
 
 // TODO: Add additional "chartbuilders" here
