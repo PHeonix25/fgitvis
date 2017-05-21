dd49d4e6ccb63b03e9ac068d1aa97b6cba269c85
PHeonix25 <p@hermens.com.au>
2017-05-21 19:55:54 +0200

    Makes requesting UserInput more functional

--- fgitvis.Console/UserInput.fs
+++ fgitvis.Console/UserInput.fs
@@ -7,19 +7,24 @@ open Fake
 let requestMenuItem () =
     printfn ""
     printfn "Please choose what type of report you'd like to see: "
-    for option in menuOptions do
-        printfn "\t %s - %s" option.Option option.Description
+
+    let displayText (option: MenuOption) = (printfn "\t %s - %s" option.Option option.Description)
+    List.iter displayText menuOptions
     printfn "";
 
     let userInput = UserInputHelper.getUserInput "Please choose a menu item: "
     if (String.IsNullOrWhiteSpace userInput) then
-        failwith "You need to choose a valid menu option"
+        failwith "You need to choose a valid menu option."
     
     let inputMatchesMenu input (menu: MenuOption) = 
         String.Equals(input, menu.Option, StringComparison.CurrentCultureIgnoreCase)
-    let menuResult = List.find (inputMatchesMenu userInput) menuOptions
+
+    let result = 
+        match List.tryFind (inputMatchesMenu userInput) menuOptions with
+        | None -> failwith "Sorry, please choose one of the character prefixes for your required report."
+        | Some result -> result
     
-    menuResult;
+    result;
 
 let requestFileLimit =
     let userInput = UserInputHelper.getUserInput "Please enter a numeric limit to the number of files we should graph: "
